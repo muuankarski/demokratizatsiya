018a2e4e4b266c06cec772e050f99261c5c0e233
Markus Kainu <markuskainu@gmail.com>
2014-04-17 13:16:22 +0300

    article updated

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@
 cd ~/workspace/russia/demokratizatsiya
 curl -o input.md http://pad.okfn.org/p/Demokratizatsiya/export/txt
 
-pandoc -s -S --number-section --toc --from=markdown+yaml_metadata_block -H /home/aurelius/workspace/web/css/rmarkdown.css input.md -o article_demokr.html
+pandoc -s -S --number-section --toc --from=markdown+yaml_metadata_block -H /home/aurelius/web/css/rmarkdown.css input.md -o article_demokr.html
 # pdf
 pandoc --toc --number-section --latex-engine=xelatex -V lang=english -V papersize:a4paper -V documentclass=scrartcl input.md -o article_demokr.pdf
 # word
