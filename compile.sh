e0f333993736466a7bd911347171bba15c84a53e
Markus Kainu <markuskainu@gmail.com>
2014-05-08 10:16:58 +0300

    article updated

--- compile.sh
+++ compile.sh
@@ -9,7 +9,7 @@ curl -o input.md http://pad.okfn.org/p/Demokratizatsiya/export/txt
 # pdf
 pandoc --toc --number-section --latex-engine=xelatex -V lang=english -V papersize:a4paper -V documentclass=scrartcl input.md -o article_demokr.pdf
 # word
-#pandoc --toc --number-section input.md -o article_demokr.docx
+pandoc --toc --number-section input.md -o article_demokr.docx
 
 git commit -am "article updated"
 git push
